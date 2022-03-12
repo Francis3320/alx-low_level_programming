13dcb6155c080c6aedb7af88d17e1950b5c0ab09
Francis3320 <darkofrancis18@yahoo.com>
2022-03-11 18:47:04 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main -main block
  * return:0
  */
 int main(void)
 {
-puts("Programming is building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
